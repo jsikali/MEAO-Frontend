ce2764cea271e2b13917f289d50bf89bbbd5e9e8
jsikali <120147133+jsikali@users.noreply.github.com>
2025-03-18 22:00:00 -0400

    notions of a plan of chat scaffolding

---
+++ meao/src/Chat.js
@@ -0,0 +1,22 @@
+export default ChatMenu; //how the frick frack pattywhack do you export multiple functions. um. erm?!
+
+function ChatMenu()
+{
+   return
+   (
+      // actually implement like. having the bit where you see messages
+      alert("chat menu in use waow")
+      //this should like. be given the ppl. and user maybe i guess. so then query db for those messages
+   )
+}
+
+function ChatSelection()
+{
+   return
+   (
+      // this would be for 1-1 and 1-n, choose which chat menu to open
+      alert("chat selection whatttt waowww")
+   )
+}
+
+// uh. also have like message imput i think. and um. um. work ending bye!
