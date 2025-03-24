8e6298fc95c369c8c757f0cd97f0a19f241e0bda
kallista <148663001+rhit-zhangks@users.noreply.github.com>
2025-03-24 01:52:19 -0400

    basic setup

--- meao/src/App.jsx
+++ meao/src/App.jsx
@@ -1,34 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Row, Col, Divider } from "antd";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const ChatBox = ({ header }) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{
+      backgroundColor:'green',
+      borderRadius:'10%'
+    }}>
+      <h1>
+        { header }
+      </h1>
+      <div style={{
+        height:''
+      }}>
+        chat content
+        chat content
+        chat content
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div style={{
+      width:'100vw',
+      backgroundColor: 'orange',
+      align:'left'
+    }}>
+      <Row justify="space-evenly" style={{
+        width:'100%',
+        backgroundColor: 'purple',
+        height: '30vh'
+      }}>
+        <Col span={7} style={{
+          backgroundColor: 'red'
+        }}>
+          settings
+        </Col>
+        <Col span={14} style={{
+
+        }}>
+          =^-.-^=
+        </Col>
+      </Row>
+      
+      <Divider orientation="left" plain>
+      </Divider>
+
+      <Row justify="space-around"style={{
+        width:'100%',
+        backgroundColor: 'aqua',
+        height: '60vh'
+      }}>
+        <Col span={7} className="chat-div">
+          <ChatBox header="one-to-one">
+          </ChatBox>
+        </Col>
+        <Col span={7} className="chat-div">
+          one-to-many
+        </Col>
+        <Col span={7} className="chat-div">
+          one-to-all
+        </Col>
+      </Row>
+    </div>
+    
   )
 }
 
