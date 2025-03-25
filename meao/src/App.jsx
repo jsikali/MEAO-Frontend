18b08bfa44c013a558ef892ddc2ab7fce5ac13a0
rhit-zhangks <zhangks@rose-hulman.edu>
2025-03-25 18:48:13 -0400

    chat select

--- meao/src/App.jsx
+++ meao/src/App.jsx
@@ -1,50 +1,59 @@
 import { useState } from 'react'
-import { Row, Col, Divider } from "antd";
+import { Row, Col, Divider, ConfigProvider } from "antd";
 import './App.css'
 import PrivateChatBox from './PrivateChatBox.jsx'
 
 function App() {
   return (
-    <div style={{
-      width:'100vw',
-      align:'left'
-    }}>
-      <Row justify="space-evenly" style={{
-        width:'100%',
-        height: '30vh'
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#33343d',
+        },
+      }}
+    >
+      <div style={{
+        width: '100vw',
+        align: 'left'
       }}>
-        <Col span={7} style={{
-          backgroundColor: 'red'
+        <Row justify="space-evenly" style={{
+          width: '100%',
+          height: '20vh'
         }}>
-          settings
-        </Col>
-        <Col span={14} style={{
+          <Col span={7} style={{
+          }}>
+            settings
+          </Col>
+          <Col span={14} style={{
+            fontSize: '100px',
+            textAlign: 'center'
+          }}>
+            =^-.-^=
+          </Col>
+          {/* <Col span={7} style={{
+          }}>
+          </Col> */}
+        </Row>
 
+        <Divider orientation="left" plain>
+        </Divider>
+
+        <Row justify="space-around" style={{
+          width: '100%',
+          height: '70vh'
         }}>
-          =^-.-^=
-        </Col>
-      </Row>
-      
-      <Divider orientation="left" plain>
-      </Divider>
-
-      <Row justify="space-around"style={{
-        width:'100%',
-        height: '60vh'
-      }}>
-        <Col span={7} className="chat-div">
-          <PrivateChatBox>
-          </PrivateChatBox>
-        </Col>
-        <Col span={7} className="chat-div">
-          one-to-many
-        </Col>
-        <Col span={7} className="chat-div">
-          one-to-all
-        </Col>
-      </Row>
-    </div>
-    
+          <Col span={7} className="chat-div">
+            <PrivateChatBox>
+            </PrivateChatBox>
+          </Col>
+          <Col span={7} className="chat-div">
+          </Col>
+          <Col span={7} className="chat-div">
+            one-to-all
+          </Col>
+        </Row>
+      </div>
+    </ConfigProvider>
   )
 }
 
--- meao/src/PrivateChatBox.jsx
+++ meao/src/PrivateChatBox.jsx
@@ -1,45 +1,64 @@
 import React, { useState } from 'react';
 import { Button, Drawer, theme } from 'antd';
+import { LeftOutlined } from '@ant-design/icons';
 
+const getChatButtons = num => {
+    let content = [];
+    for (let i = 1; i <= num; i++) {
+        const item = <Button type="primary" onClick={() => setIsVisible(!isVisible)} style={{
+            borderRadius: 0,
+            width: '100%'
+        }}>
+            chat # {i}
+        </Button>
+        content.push(item);
+    }
+    return content;
+};
 
 const PrivateChatBox = () => {
-  const { token } = theme.useToken();
-  const [open, setOpen] = useState(false);
-  const showDrawer = () => {
-    setOpen(true);
-  };
-  const onClose = () => {
-    setOpen(false);
-  };
+    const { token } = theme.useToken();
+    const [isVisible, setIsVisible] = useState(true);
 
-  const containerStyle = {
-    height: '100%',
-    overflow: 'auto',
-    padding: '10%',
-    backgroundColor: '#c2c6E8',
-    borderRadius: '16px',
-    boxSizing: 'border-box',
-  };
+    const containerStyle = {
+        height: '100%',
+        overflow: 'auto',
+        padding: '0 5%',
+        backgroundColor: '#c2c6E8',
+        borderRadius: '16px',
+        boxSizing: 'border-box',
+    };
 
-  return (
-    <div style={containerStyle}>
-      Render in this
-      <div style={{ marginTop: 16 }}>
-        <Button type="primary" onClick={showDrawer}>
-          Open
-        </Button>
-      </div>
-      <Drawer
-        title="Choose Chat"
-        placement="left"
-        closable={false}
-        onClose={onClose}
-        open={open}
-        getContainer={false}
-      >
-        <p>Some contents...</p>
-      </Drawer>
-    </div>
-  );
+    return (
+        <div style={containerStyle}>
+            <div style={{
+                marginTop: '5%',
+                height: '10%',
+            }}>
+                {isVisible &&
+                    <Button type="primary" icon={<LeftOutlined />} onClick={() => setIsVisible(!isVisible)} style={{
+                        borderRadius: '10px'
+                    }}>
+                        select chat
+                    </Button>
+                }
+
+            </div>
+            <div style={{
+                height: '58vh',
+                overflowX: 'hidden',
+                overflowY: 'auto',
+            }}>
+                <div style={{
+                    backgroundColor: 'blue'
+                }}>
+
+                    { getChatButtons(20) }
+
+
+                </div>
+            </div>
+        </div>
+    );
 };
 export default PrivateChatBox;
