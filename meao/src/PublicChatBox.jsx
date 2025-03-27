df884a1bb333b27db1e92b771470e29840707315
kallista <148663001+rhit-zhangks@users.noreply.github.com>
2025-03-27 14:36:02 -0400

    scrolling and css changes

--- meao/src/PublicChatBox.jsx
+++ meao/src/PublicChatBox.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, /*theme*/ } from 'antd';
-import Messages from './Messages.jsx'
+import Messages from './Messages.jsx';
 import axios from 'axios';
 import MessageSendButton from './MessageSendButton.jsx';
 
 const PublicChatBox = ({ getIsLoggedIn }) => {
     const [messages, setMessages] = useState([]);
+    const messagesEndRef = useRef(null); // Ref for scrolling
 
     useEffect(() => {
         const fetchMessages = () => {
@@ -22,15 +23,14 @@ const PublicChatBox = ({ getIsLoggedIn }) => {
         fetchMessages();
         const interval = setInterval(fetchMessages, 1000);
         return () => clearInterval(interval);
-    });
+    }, []);
 
-    const messageJSON = () => {
-        console.log("getting messages");
-        axios({
-            method: 'get',
-            url: 'http://137.112.221.75:5000/messages/public'
-        }).then(res => res.data);
-    }
+    // Scroll to bottom when messages update
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, [messages]);
 
     const containerStyle = {
         height: '100%',
@@ -43,12 +43,8 @@ const PublicChatBox = ({ getIsLoggedIn }) => {
 
     return (
         <div style={containerStyle}>
-            <div style={{
-                marginTop: '2vh',
-                height: '5vh',
-            }}>
+            <div style={{ marginTop: '2vh', height: '5vh' }}>
                 <h2>all chat!</h2>
-
             </div>
             <div style={{
                 height: '60vh',
@@ -61,28 +57,39 @@ const PublicChatBox = ({ getIsLoggedIn }) => {
                     minHeight: '100%',
                     overflow: 'hidden'
                 }}>
-                    <div style={{
-                        padding: '10px'
-                    }}>
-
-                        {messages.map((msg, index) => (
-                            <div key={index} style={{}}>
+                    <div style={{ padding: '10px' }}>
+                        {messages.slice().reverse().map((msg, index) => (
+                            <div key={index} style={{ backgroundColor: 'white',
+                                padding: '5px',
+                                margin: '5px',
+                                borderRadius: '5px'
+                             }}>
                                 <div style={{}}>
-                                    <span style={{}}>{msg.sender_id}</span>
-                                    <span style={{}}>{new Date(msg.timestamp).toLocaleString()}</span>
+                                    <span style={{
+                                        color: '#33343d'
+                                    }}>{msg.sender_id} </span>
+                                    <span style={{
+                                        color: '#7B7D93'
+                                    }}> {new Date(msg.timestamp).toLocaleString()}</span>
                                 </div>
-                                <p style={{}}>{msg.content}</p>
+                                <p style={{
+                                    color: '#33343d'
+                                }}>{msg.content}</p>
                             </div>
                         ))}
+                        {/* Empty div to help scroll to bottom */}
+                        <div ref={messagesEndRef}></div>
                         {/* have the input for sending */}
                         <input type="text" id='publicchat' />
                         <MessageSendButton
                             chatBoxID='publicchat'
-                            getIsLoggedIn={getIsLoggedIn}></MessageSendButton>
+                            getIsLoggedIn={getIsLoggedIn}>
+                        </MessageSendButton>
                     </div>
                 </div>
             </div>
         </div>
     );
 };
+
 export default PublicChatBox;
