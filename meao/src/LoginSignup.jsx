53bddd61e89f40ceedf00533b84fcd9a6e66486e
jsikali <120147133+jsikali@users.noreply.github.com>
2025-03-27 00:13:28 -0400

    github down erm erm frown

--- meao/src/LoginSignup.jsx
+++ meao/src/LoginSignup.jsx
@@ -1,92 +1,88 @@
-//import { useState } from "react";
-import { LeftOutlined } from "@ant-design/icons";
+//import React, { useState } from 'react';
 import { Button } from "antd";
-import SignUpButton from "./SignUpButton.jsx";
-import LoginButton from "./LoginButton.jsx";
-var toggle = false;
+import { LeftOutlined } from "@ant-design/icons";
+import SignUpButton from "./SignUpButton.jsx"
+import LoginButton from "./LoginButton.jsx"
+//import { useState } from 'react';
 
-//const [isSelectingChat] = useState(true);
+const Login = ({ setIsLoggedIn, getIsLoggedIn }) => {
+  //const { token } = theme.useToken();
+  var toggle = false;
 
-const swapButtons = () => {
-  toggle = !toggle;
-  console.log("swapping to ", toggle);
-  //import { useState } from 'react';
-}
-  const LoginSignup = ({ setIsLoggedIn, getIsLoggedIn }) => {
-    //const { token } = theme.useToken();
+  const swapButtons = () => {
+    toggle = !toggle;
+    console.log("swapping to ", toggle);
+    if (toggle)
+    {
+      return <LoginButton></LoginButton>
+    }else{
+      return <SignUpButton></SignUpButton>
+    }
+  };
 
-    const sendLoginDetails = (user, pass) => {
-      console.log("actually handle this without leaking details lol");
-      //for the sake of being really funny
-      console.log(
-        `omg the username ${user} and password ${pass} may or may not work!!`
-      );
-      setIsLoggedIn(!getIsLoggedIn()); //just toggle for now
-    };
+  const containerStyle = {
+    height: "100%",
+    overflow: "auto",
+    padding: "0 5%",
+    backgroundColor: "#c2c6E8",
+    borderRadius: "16px",
+    boxSizing: "border-box",
+  };
 
-    const containerStyle = {
-      height: "100%",
-      overflow: "auto",
-      padding: "0 5%",
-      backgroundColor: "#c2c6E8",
-      borderRadius: "16px",
-      boxSizing: "border-box",
-    };
-
-    return (
-      <div style={containerStyle}>
-        <div
-          style={{
-            marginTop: "2vh",
-            height: "3vh",
-          }}
-        >
-          {getIsLoggedIn() ? (
-            <h2
+  return (
+    <div style={containerStyle}>
+      <div
+        style={{
+          marginTop: "2vh",
+          height: "3vh",
+        }}
+      >
+        {getIsLoggedIn() ? (
+          <h2
+            style={{
+              color: "#33343d",
+              marginLeft: "8px",
+            }}
+          >
+            logged in!!
+          </h2>
+        ) : (
+          <>
+            <div
+              id="username"
               style={{
                 color: "#33343d",
                 marginLeft: "8px",
               }}
             >
-              logged in!!
-            </h2>
-          ) : (
-            <>
-              <div
-                style={{
-                  color: "#33343d",
-                  marginLeft: "8px",
-                }}
-              >
-                login/signup
-              </div>
-            </>
-          )}
-        </div>
-
-        <div
-          style={{
-            height: "5vh",
-            borderRadius: "10px",
-          }}
-        >
-          <input type="text" id="username" />
-          <input type="text" id="password" />
-        </div>
-        <Button
-          type="primary"
-          icon={<LeftOutlined />}
-          onClick={() => swapButtons()}
-          style={{
-            borderRadius: "10px",
-          }}
-        >
-          toggle
-        </Button>
-        {toggle ? <LoginButton></LoginButton> : <SignUpButton></SignUpButton>}
+              login/sign up!!
+            </div>
+          </>
+        )}
       </div>
-    );
-  };
 
-  export default LoginSignup;
+      <div
+        style={{
+          height: "5vh",
+          borderRadius: "10px",
+        }}
+      >
+        <input type="text" id='username' />
+        <input type="text" id='password' />
+      </div>
+      <Button
+        type="primary"
+        icon={<LeftOutlined />}
+        onClick={() => swapButtons()}
+        style={{
+          borderRadius: "10px",
+        }}
+      >
+        toggle
+      </Button>
+      <div>{swapButtons()}</div>
+    </div>
+  );
 };
+
+export default Login;
