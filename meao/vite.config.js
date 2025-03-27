3b2ffa455cae70f337fdac084fcff1c1aefc89a3
Joshua Lowe <lowej2@rose-hulman.edu>
2025-03-27 07:21:29 +0000

    should be fixed oops

--- meao/vite.config.js
+++ meao/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), basicSsl()],
 })
